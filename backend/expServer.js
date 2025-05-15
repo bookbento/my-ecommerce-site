fe256e2caea47fd71009fb2753998ca0c73cd0fb
bookbento <mrsarunpatbook3@gmail.com>
2025-05-15 16:31:13 +0700

    assignment beta 2

--- backend/expServer.js
+++ backend/expServer.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use('/api/subject', require('./routes/subject'));
 app.use('/api/contact', require('./routes/contact.'));
 app.use('/api/subscribe',require('./routes/subscribe'));
+app.use('/api/register',require('./routes/register'));
 
 
 app.listen(POST, () => {
